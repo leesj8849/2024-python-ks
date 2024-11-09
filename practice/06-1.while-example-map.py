c6b8d30983d206c93637fd7d1fe28333edafc807
User <hyun88945@gmail.com>
2024-11-09 11:45:31 +0900

    while

--- practice/06-1.while-example-map.py
+++ practice/06-1.while-example-map.py
@@ -1,27 +1,25 @@
-X = ""
-Y = ""
-X = 0
-Y = 0
+x = 0
+y = 0
 a = ""
 a = str(input("어디로 이동하시겠습니까"))
 while a != "UP" or "DOWN" or "LEFT" or "RIGHT":
     input("어디로 이동하시겠습니까")
 if a == "UP":
-  X = X
-  Y = Y + 1
-  print((X,Y))
+  x = x
+  y = y + 1
+  print(x,y)
 
 if a == "DOWN":
-  X = X
-  Y = Y - 1
-  print((X,Y))
+  x = x
+  y = y - 1
+  print(x,y)
 
 if a == "LEFT":
-    X = X - 1
-    Y = Y
-    print((X,Y))
+    x = x - 1
+    y = y
+    print(x,y)
 
 if a == "RIGHT":
-  X = X + 1
-  Y = Y
-  print((X,Y))
+  x = x + 1
+  y = y
+  print(x,y)
