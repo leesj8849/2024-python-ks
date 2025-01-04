324ee464f8832dbd28fbc6f256f82de3f925747e
10511이서준 <hyun88945@gmail.com>
2025-01-04 11:40:41 +0900

    .

--- practice/05.py
+++ practice/05.py
@@ -1,3 +1,7 @@
-x=2
-y=0
-for {}
+print(chr(65))
+print(chr(90))
+import random
+alphabet = []
+
+for i in range(0,10):
+    alphabet.append(chr(random.randrange(65,91)))
---
+++ practice/11.py
@@ -0,0 +1,3 @@
+num = int(input)
+for n in range(10):
+    print(num,"*", n,"=",(num*n))
